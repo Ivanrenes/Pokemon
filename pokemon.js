3a59f003e288d8697c3ed207d8a6c1ebd95781c2
Hector Cohen <hcohen6515@cues.edu.co>
2020-12-02 14:00:57 -0500

    get pokemon by id or name

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,17 @@
-console.log('funcionando')
+
 const button = document.querySelector('#button')
 button.addEventListener('click', ()=>{
-	console.log('funcionando')
+    
+    let caja = document.getElementById('caja').value;
+    let imgdata= document.getElementById('img');
+    let p = document.getElementById('info');
+    
+    const uri = `https://pokeapi.co/api/v2/pokemon/${caja}`
+      fetch(uri).then(response => response.json())
+	  .then(data =>{
+	      const img = new Image;
+	      imgdata.src = data.sprites.front_shiny
+	    p.innerHTML = data.species.name
+	  })
 })
+
